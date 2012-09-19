23476095c676ea7d8acb1a782d1bacdf90d0b3cd
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2012-09-18 22:10:10 -0700

    Tests for posting threads/responses/comments and voting on a thread.

--- edx_specs/discussion.rb
+++ edx_specs/discussion.rb
@@ -3,14 +3,14 @@ require './spec_helper'
 describe "Discussion", :type => :request do
 
   subject { page }
-  let(:thread_data){ {body: Faker::Lorem.paragraph(2), title: Faker::Lorem.sentence(5), topic: "General"} }
+  let(:thread_data){ generate_post }
 
   steps "Discussion Forum View" do
 
     it "should let you log in" do
       log_in
       goto_course "BerkeleyX/CS188/fa12"
-      click_link "Discussion" 
+      click_link "Discussion"
     end
 
     it "should have a list of threads" do
@@ -40,13 +40,56 @@ describe "Discussion", :type => :request do
       new_first_thread_list_item = page.find('.post-list .list-item:first')
       new_first_thread_list_item.should have_content (thread_data[:title])
       new_first_thread_list_item.should_not have_content old_first_thread_title
+
+      thread_container = page.find('article.discussion-article .thread-content-wrapper')
+      thread_container.should have_content thread_data[:body]
+      thread_container.should have_content thread_data[:title]
+      thread_data[:id] = page.find('article.discussion-article')['data-id']
     end
 
-    it "should show the thread's body when a thread is clicked" do 
-      page.find('.post-list .list-item:first').click
+    it "should show the thread's body when a thread is clicked" do
+      # Navigate to index first
+      goto_course "BerkeleyX/CS188/fa12"
+      click_link "Discussion"
+
+      page.find('.post-list .list-item:first a').click
       page.find('.new-post-article').should_not be_visible
+
+      thread_container = page.find('article.discussion-article .thread-content-wrapper')
+      thread_container.should have_content thread_data[:body]
+      thread_container.should have_content thread_data[:title]
     end
 
+    it "should show the thread as followed" do
+      thread_container = page.find('article.discussion-article .thread-content-wrapper')
+      thread_container.should have_selector('.is-followed')
+    end
+
+    it "should let you respond to a post" do
+      response_form = page.find('.discussion-reply-new')
+      spammy_comment = Faker::Company.bs
+      fill_in_wmd_body response_form, spammy_comment
+      response_form.find('.discussion-submit-post').click
+      responses = page.find('.responses')
+      responses.should have_content spammy_comment
+    end
+
+    it "should let you post a comment on a response" do
+      comment = "Help! I've been hacked to produce spam!"
+      first_response = page.find('.responses li:first')
+      first_response.find('.wmd-input').click
+      fill_in_wmd_body(first_response, comment)
+      first_response.find('.discussion-submit-comment').click
+      first_response.find('.wmd-input').should_not have_content(comment)
+      first_response.find('.comments .response-body').should have_content(comment)
+    end
+
+    it "should let you vote on a post" do
+      vote_btn = page.find('article.discussion-article .thread-content-wrapper a.vote-btn')
+      vote_btn.find('.votes-count-number').should have_content '0'
+      vote_btn.click
+      vote_btn.find('.votes-count-number').should have_content '1'
+    end
   end
 
 end
--- edx_specs/helpers.rb
+++ edx_specs/helpers.rb
@@ -14,3 +14,18 @@ end
 def fill_in_wmd_body(container, body)
   container.find("textarea.wmd-input").set(body)
 end
+
+def wait_for_ajax(timeout = Capybara.default_wait_time)
+  # From http://artsy.github.com/blog/2012/02/03/reliably-testing-asynchronous-ui-w-slash-rspec-and-capybara/
+  page.wait_until(timeout) do
+    page.evaluate_script 'jQuery.active == 0'
+  end
+end
+
+def generate_post
+  post = {body: Faker::Lorem.paragraph(2), topic: "General"}
+  post[:hash] = rand.hash.to_s(36)
+  # Stick something random in the title to search for later
+  post[:title] = "#{Faker::Lorem.sentence(5)} (#{post[:hash]})"
+  post
+end
